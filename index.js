d1c20325d48abe2d33fc70326b1f97917bded4a7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-28 16:51:35 +0200

    feat: :sparkles: add text showing which file the log was triggered from

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ import kleur from "kleur";
 const originalInfo = console.info;
 const originalWarn = console.warn;
 const originalError = console.error;
+const originalLog = console.log;
+
+function getInitiator(e) {
+  let initiator = "unknown place";
+  if (typeof e.stack === "string") {
+    let isFirst = true;
+    for (const line of e.stack.split("\n")) {
+      const matches = line.match(/^\s+at\s+(.*)/);
+      if (matches) {
+        if (!isFirst) {
+          // first line - current function
+          // second line - caller (what we are looking for)
+          initiator = matches[1].split("(")[1];
+          return initiator;
+        }
+        isFirst = false;
+      }
+    }
+  }
+}
 
 console.info = (...args) => {
+  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
+  const e = new Error();
+  const initiator = getInitiator(e);
+
   const blueText = kleur.blue().bold;
   const formattedArgs = args.map((arg) => {
     if (typeof arg === "string") {
@@ -13,10 +37,15 @@ console.info = (...args) => {
       return arg;
     }
   });
+  formattedArgs.push(kleur.italic(`(${initiator})`));
   originalInfo.apply(console, formattedArgs);
 };
 
 console.warn = (...args) => {
+  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
+  const e = new Error();
+  const initiator = getInitiator(e);
+
   const yellowText = kleur.yellow().bold;
   const formattedArgs = args.map((arg) => {
     if (typeof arg === "string") {
@@ -25,10 +54,15 @@ console.warn = (...args) => {
       return arg;
     }
   });
+  formattedArgs.push(kleur.italic(`(${initiator})`));
   originalWarn.apply(console, formattedArgs);
 };
 
 console.error = (...args) => {
+  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
+  const e = new Error();
+  const initiator = getInitiator(e);
+
   const redText = kleur.red().bold;
   const formattedArgs = args.map((arg) => {
     if (typeof arg === "string") {
@@ -37,7 +71,24 @@ console.error = (...args) => {
       return arg;
     }
   });
+  formattedArgs.push(kleur.italic(`(${initiator})`));
   originalError.apply(console, formattedArgs);
+};
+
+console.log = (...args) => {
+  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
+  const e = new Error();
+  const initiator = getInitiator(e);
+
+  const formattedArgs = args.map((arg) => {
+    if (typeof arg === "string") {
+      return arg;
+    } else {
+      return arg;
+    }
+  });
+  formattedArgs.push(kleur.italic(`(${initiator})`));
+  originalLog.apply(console, formattedArgs);
 };
 
 export default console;
