415ab3369b164bd2eceadbc81489240365458fb4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-05 17:53:57 +0100

    feat: support loops, make path shorter

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ function getInitiator(e) {
         if (!isFirst) {
           // first line - current function
           // second line - caller (what we are looking for)
-          initiator = matches[1].split("(")[1];
+          if (matches[1].split("(").length === 1) {
+            // inside loop or something
+            initiator = matches[1].split("(")[0];
+          } else if (matches[1].split("(").length === 2) {
+            // not inside loop
+            initiator = matches[1].split("(")[1];
+          } else {
+            initiator = "something went wrong";
+          }
+          // remove long path info, split at last /
+          initiator = initiator.split("/")[initiator.split("/").length - 1];
           return initiator;
         }
         isFirst = false;
@@ -23,6 +33,22 @@ function getInitiator(e) {
     }
   }
 }
+
+console.log = (...args) => {
+  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
+  const e = new Error();
+  const initiator = getInitiator(e);
+
+  const formattedArgs = args.map((arg) => {
+    if (typeof arg === "string") {
+      return arg;
+    } else {
+      return arg;
+    }
+  });
+  formattedArgs.push(kleur.italic(`(${initiator}`));
+  originalLog.apply(console, formattedArgs);
+};
 
 console.info = (...args) => {
   // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
@@ -73,22 +99,6 @@ console.error = (...args) => {
   });
   formattedArgs.push(kleur.italic(`(${initiator}`));
   originalError.apply(console, formattedArgs);
-};
-
-console.log = (...args) => {
-  // https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs
-  const e = new Error();
-  const initiator = getInitiator(e);
-
-  const formattedArgs = args.map((arg) => {
-    if (typeof arg === "string") {
-      return arg;
-    } else {
-      return arg;
-    }
-  });
-  formattedArgs.push(kleur.italic(`(${initiator}`));
-  originalLog.apply(console, formattedArgs);
 };
 
 export default console;
